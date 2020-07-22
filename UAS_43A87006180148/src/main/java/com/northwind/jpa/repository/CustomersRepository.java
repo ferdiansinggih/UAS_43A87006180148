68082e870eb9e9a727b0e53bccae356994597d1f
KokoRiyanto <63348728+KokoRiyanto@users.noreply.github.com>
2020-07-22 04:56:14 -1200

    Update CustomersRepository.java

--- UAS_43A87006180148/src/main/java/com/northwind/jpa/repository/CustomersRepository.java
+++ UAS_43A87006180148/src/main/java/com/northwind/jpa/repository/CustomersRepository.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Repository;
 
 /**
  *
- * @author Rizukiwahyudi
+ * @author User
  */
 @Repository
 public interface CustomersRepository extends JpaRepository<Customers, String>{    
